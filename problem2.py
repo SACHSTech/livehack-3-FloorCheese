3769a8751f42f1e6f127b9cdead45d0a0e1cb8fc
Eric Fabroa <>
2021-04-17 17:26:12 +0000

    submit

--- problem2.py
+++ problem2.py
@@ -1,1 +1,11 @@
+travel_distance = int(input("Enter distance here"))
 
+number_of_days = 0
+total_distance = total_distance + travel_distance
+
+if total_distance < 100:
+  travel_distance = int(input("Enter distance here"))
+  number_of_days = number_of_days + 1
+else:
+  print("It took " + number_of_days + " days to surpass 100km driven.")
+  
